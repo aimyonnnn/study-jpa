41af6548b8adf45386f7bb55b3e88f16c98f654b
SUMIN PARK <lovepoemlovepoem0516@gmail.com>
2023-10-01 15:07:06 +0900

    JPQL과 네이티브 쿼리

--- src/main/java/com/fastcampus/ch4/BoardRepository.java
+++ src/main/java/com/fastcampus/ch4/BoardRepository.java
@@ -1,11 +1,24 @@
 package com.fastcampus.ch4;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
+    
+    @Query("select b from Board b") // JPQL은 명칭을 대소문자 구분에 주의
+    List<Board> findAllBoard(); // 메서드 이름은 아무거나 해도 상관없음.
+
+    @Query("select b from Board b where b.title=?1 and b.writer=?2")
+    List<Board> findByTitleAndWriter2(String title, String writer);
+    
+    @Query(value = "select * from board", nativeQuery = true) // SQL, nativeQuery default는 false임
+    List<Board> findAllBoardBySQL();
+    
+    @Query(value = "select title, writer from board", nativeQuery = true)
+    List<Object[]> findAllBoardBySQL2(); // 컬럼을 따로 조회할 땐 Object[] 배열로 받아야함.
 
     // select count(*) from board where writer = :writer
     int countAllByWriter(String writer);
